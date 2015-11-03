4b0bd7d05749994751293a3040d5163727ddbfd8
Matt Raporte <matt-raporte@users.noreply.github.com>
2015-11-03 17:59:47 -0500

    changed POR to call Management.addRental in it's endPOS

--- src/Management.java
+++ src/Management.java
@@ -156,16 +156,16 @@ public class Management {
              dayTwo = (Calendar) day2.clone();
 
 
-	    if (dayOne.get(Calendar.YEAR) == dayTwo.get(Calendar.YEAR)) {
-	        return (dayTwo.get(Calendar.DAY_OF_YEAR) - dayOne.get(Calendar.DAY_OF_YEAR));
-	    } else {
-	        if (dayTwo.get(Calendar.YEAR) > dayOne.get(Calendar.YEAR)) {
-	            //swap them
-	            Calendar temp = dayOne;
-	            dayOne = dayTwo;
-	            dayTwo = temp;
-	        }
-	        int extraDays = 0;
+     if (dayOne.get(Calendar.YEAR) == dayTwo.get(Calendar.YEAR)) {
+         return (dayTwo.get(Calendar.DAY_OF_YEAR) - dayOne.get(Calendar.DAY_OF_YEAR));
+     } else {
+         if (dayTwo.get(Calendar.YEAR) > dayOne.get(Calendar.YEAR)) {
+             //swap them
+             Calendar temp = dayOne;
+             dayOne = dayTwo;
+             dayTwo = temp;
+         }
+         int extraDays = 0;
 
 
          int dayOneOriginalYearDays = dayOne.get(Calendar.DAY_OF_YEAR);
@@ -196,7 +196,7 @@ public class Management {
    }
  }
  
- public static void addRental(long phone, List <Integer> rentalList)
+ public static void addRental(long phone, List <Item> rentalList)
  {
   long nextPhone = 0;
   List <String> fileList = new ArrayList<String>();
@@ -227,42 +227,10 @@ public class Management {
           } 
           if(nextPhone == phone)//finds the user in the database
           { 
-           //modifiedLine = line.split(" ")[0];
-           /*
-     if(line.split(" ").length >1)
-           {
-               
-     
-                   for(int i =1; i<line.split(" ").length; i++) 
-                   {
-                  String returnedBool = (line.split(" ")[i]).split(",")[2];
- 
-                     boolean b = returnedBool.equalsIgnoreCase("true");
-                     if (!b)//if item wasn't returned already
-                     { 
-                      for (returnCounter = 0 ; returnCounter < returnedList.size() ; returnCounter++) 
-                       if (Integer.parseInt(line.split(" ")[i].split(",")[0]) == returnedList.get(returnCounter).getItemID())
-                       {
-                        modifiedLine += " " + line.split(" ")[i].split(",")[0] + "," + dateFormat + "," + "true";
-          
-                       }
-                      if (returnCounter == returnedList.size() )
-                       modifiedLine += line.split(" ")[i]; //not returning this item now
-                     }
-                   
-                     
-                     else
-                     {
-                      modifiedLine += " " + line.split(" ")[i];
-                     }
-                   }
-       
-       
-           }*/
-     
+           
      //loop through each "ID" in rentalList
-     for (Integer id : rentalList){
-      line = line + " "+ id + ","+dateFormat+","+"false";
+     for (Item item : rentalList){
+      line = line + " "+ item.getItemID() + ","+dateFormat+","+"false";
      }
      
            fileList.add(line);
@@ -322,94 +290,94 @@ public class Management {
      
      //Reads from file to read the changes to make:
 
-	 try{
-		   ableToRead = true;
-	       FileReader fileR = new FileReader(userDatabase);
-	       BufferedReader textReader = new BufferedReader(fileR);
-	       String line;
-	       int returnCounter = 0;
-	       //reads the entire database
-	       line = textReader.readLine(); //skips the first line, which explains how the DB is formatted. 
-	       fileList.add(line); //but stores it since it is the first line of the DB
-	       while ((line = textReader.readLine()) != null)
-	       {
-	        
-	         try {  
-	           nextPhone = Long.parseLong(line.split(" ")[0]);    
-	         } catch (NumberFormatException e) {  
-	           continue;  
-	         } 
-	         if(nextPhone == phone)//finds the user in the database
-	         { 
-	        	 modifiedLine = line.split(" ")[0];
-	        	 if(line.split(" ").length >1)
-	        	 {
-	              
-	                  for(int i =1; i<line.split(" ").length; i++)
-	                  {
-	                	String returnedBool = (line.split(" ")[i]).split(",")[2];
-	
-	                    boolean b = returnedBool.equalsIgnoreCase("true");
-	                    if (!b)//if item wasn't returned already
-	                    { 
-	                    	for (returnCounter = 0 ; returnCounter < returnedList.size() ; returnCounter++) 
-	                    		if (Integer.parseInt(line.split(" ")[i].split(",")[0]) == returnedList.get(returnCounter).getItemID())
-	                    		{
-	                    			modifiedLine += " " + line.split(" ")[i].split(",")[0] + "," + dateFormat + "," + "true";
-	         
-	                    		}
-	                    	if (returnCounter == returnedList.size() )
-		                     modifiedLine += line.split(" ")[i]; //not returning this item now
-	                    }
-	                  
-	                    
-	                    else
-	                    {
-	                    	modifiedLine += " " + line.split(" ")[i];
-	                    }
-	                  }
-	        	 }
-	        	 fileList.add(modifiedLine);
-	         }
-	         else
-	        	 fileList.add(line); //adds the lines that are not modified from the database to the list to be rewritten later
-	        	 
-	      
-	       }
-	       textReader.close();
-	       fileR.close();
-	 }
-	   
-	   //catches exceptions
-	     catch(FileNotFoundException ex) {
-	       System.out.println("cannot open userDB"); 
-	      
-	     }
-	     catch(IOException ex) {
-	       System.out.println("ioexception");
-	     }
-	 
-	 //Now writes to file to make the changes:
-	 if (ableToRead) //if file has been read throughly
-	 {
-		 try
-			{
-				File file = new File(userDatabase);		
-				FileWriter fileR = new FileWriter(file.getAbsoluteFile());
-				BufferedWriter bWriter = new BufferedWriter(fileR);
-				PrintWriter writer = new PrintWriter(bWriter);
-				
-				for (int wCounter = 0; wCounter < fileList.size() ; ++wCounter)
-					writer.println(fileList.get(wCounter));
-				
-				bWriter.close(); //closes writer
-			}
-			
-			catch(IOException e){}
-			{
-			}
-	 }
-	 
+  try{
+     ableToRead = true;
+        FileReader fileR = new FileReader(userDatabase);
+        BufferedReader textReader = new BufferedReader(fileR);
+        String line;
+        int returnCounter = 0;
+        //reads the entire database
+        line = textReader.readLine(); //skips the first line, which explains how the DB is formatted. 
+        fileList.add(line); //but stores it since it is the first line of the DB
+        while ((line = textReader.readLine()) != null)
+        {
+         
+          try {  
+            nextPhone = Long.parseLong(line.split(" ")[0]);    
+          } catch (NumberFormatException e) {  
+            continue;  
+          } 
+          if(nextPhone == phone)//finds the user in the database
+          { 
+           modifiedLine = line.split(" ")[0];
+           if(line.split(" ").length >1)
+           {
+               
+                   for(int i =1; i<line.split(" ").length; i++)
+                   {
+                  String returnedBool = (line.split(" ")[i]).split(",")[2];
+ 
+                     boolean b = returnedBool.equalsIgnoreCase("true");
+                     if (!b)//if item wasn't returned already
+                     { 
+                      for (returnCounter = 0 ; returnCounter < returnedList.size() ; returnCounter++) 
+                       if (Integer.parseInt(line.split(" ")[i].split(",")[0]) == returnedList.get(returnCounter).getItemID())
+                       {
+                        modifiedLine += " " + line.split(" ")[i].split(",")[0] + "," + dateFormat + "," + "true";
+          
+                       }
+                      if (returnCounter == returnedList.size() )
+                       modifiedLine += line.split(" ")[i]; //not returning this item now
+                     }
+                   
+                     
+                     else
+                     {
+                      modifiedLine += " " + line.split(" ")[i];
+                     }
+                   }
+           }
+           fileList.add(modifiedLine);
+          }
+          else
+           fileList.add(line); //adds the lines that are not modified from the database to the list to be rewritten later
+           
+       
+        }
+        textReader.close();
+        fileR.close();
+  }
+    
+    //catches exceptions
+      catch(FileNotFoundException ex) {
+        System.out.println("cannot open userDB"); 
+       
+      }
+      catch(IOException ex) {
+        System.out.println("ioexception");
+      }
+  
+  //Now writes to file to make the changes:
+  if (ableToRead) //if file has been read throughly
+  {
+   try
+   {
+    File file = new File(userDatabase);  
+    FileWriter fileR = new FileWriter(file.getAbsoluteFile());
+    BufferedWriter bWriter = new BufferedWriter(fileR);
+    PrintWriter writer = new PrintWriter(bWriter);
+    
+    for (int wCounter = 0; wCounter < fileList.size() ; ++wCounter)
+     writer.println(fileList.get(wCounter));
+    
+    bWriter.close(); //closes writer
+   }
+   
+   catch(IOException e){}
+   {
+   }
+  }
+  
 
  }
  
